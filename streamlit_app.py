6536d601b52001cd2af6c9f10234eba0b411d769
djuxt <davhalf@gmail.com>
2024-07-02 14:37:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,18 +10,7 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
-def main():
-    st.title('Mi Aplicación Básica en Streamlit')
-    
-    # Pedir datos al usuario
-    user_input = st.text_input('Ingresa algún dato:')
-    
-    # Mostrar los datos ingresados
-    if user_input:
-        st.write(f'Has ingresado: {user_input}')
-    
-if __name__ == '__main__':
-    main()
+
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
@@ -42,7 +31,18 @@ df = pd.DataFrame({
     "idx": indices,
     "rand": np.random.randn(num_points),
 })
-
+def main():
+    st.title('Mi Aplicación Básica en Streamlit')
+    
+    # Pedir datos al usuario
+    user_input = st.text_input('Ingresa algún dato:')
+    
+    # Mostrar los datos ingresados
+    if user_input:
+        st.write(f'Has ingresado: {user_input}')
+    
+if __name__ == '__main__':
+    main()
 st.altair_chart(alt.Chart(df, height=700, width=700)
     .mark_point(filled=True)
     .encode(
